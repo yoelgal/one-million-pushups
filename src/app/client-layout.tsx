0269155389d4a1c5e5fc84a5ab7cbc5ef73eebb5
yoelgal <yoelgal108@gmail.com>
2025-02-03 19:59:40 +0000

    hello

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { SessionProvider } from "next-auth/react";
 
 const queryClient = new QueryClient();
 
